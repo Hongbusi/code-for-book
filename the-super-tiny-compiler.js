8fc009cc911ab02c6770981e26b8048e3d70f663
Hongbusi <q951686662@qq.com>
2022-01-18 14:14:53 +0800

    chore: tokenizer

--- the-super-tiny-compiler.js
+++ the-super-tiny-compiler.js
@@ -48,94 +48,47 @@ function tokenizer(input) {
       continue;
     }
 
-    // The next type of token is a number. This is different than what we have
-    // seen before because a number could be any number of characters and we
-    // want to capture the entire sequence of characters as one token.
-    //
     //   (add 123 456)
     //        ^^^ ^^^
-    //        Only two separate tokens
-    //
-    // So we start this off when we encounter the first number in a sequence.
     let NUMBERS = /[0-9]/;
     if (NUMBERS.test(char)) {
-
-      // We're going to create a `value` string that we are going to push
-      // characters to.
       let value = '';
-
-      // Then we're going to loop through each character in the sequence until
-      // we encounter a character that is not a number, pushing each character
-      // that is a number to our `value` and incrementing `current` as we go.
       while (NUMBERS.test(char)) {
         value += char;
         char = input[++current];
       }
-
-      // After that we push our `number` token to the `tokens` array.
       tokens.push({ type: 'number', value });
-
-      // And we continue on.
       continue;
     }
 
-    // We'll also add support for strings in our language which will be any
-    // text surrounded by double quotes (").
-    //
     //   (concat "foo" "bar")
-    //            ^^^   ^^^ string tokens
-    //
-    // We'll start by checking for the opening quote:
+    //            ^^^   ^^^
     if (char === '"') {
-      // Keep a `value` variable for building up our string token.
       let value = '';
-
-      // We'll skip the opening double quote in our token.
       char = input[++current];
-
-      // Then we'll iterate through each character until we reach another
-      // double quote.
       while (char !== '"') {
         value += char;
         char = input[++current];
       }
-
-      // Skip the closing double quote.
       char = input[++current];
-
-      // And add our `string` token to the `tokens` array.
       tokens.push({ type: 'string', value });
-
       continue;
     }
 
-    // The last type of token will be a `name` token. This is a sequence of
-    // letters instead of numbers, that are the names of functions in our lisp
-    // syntax.
-    //
     //   (add 2 4)
     //    ^^^
-    //    Name token
-    //
     let LETTERS = /[a-z]/i;
     if (LETTERS.test(char)) {
       let value = '';
-
-      // Again we're just going to loop through all the letters pushing them to
-      // a value.
       while (LETTERS.test(char)) {
         value += char;
         char = input[++current];
       }
-
-      // And pushing that value as a token with the type `name` and continuing.
       tokens.push({ type: 'name', value });
-
       continue;
     }
 
-    // Finally if we have not matched a character by now, we're going to throw
-    // an error and completely exit.
+    // 最后，如果我们现在还没有匹配到一个字符，我们将抛出一个错误并完全退出。
     throw new TypeError('I dont know what this character is: ' + char);
   }
 
