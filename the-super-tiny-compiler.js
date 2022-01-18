b534f1ab1121d8e45c139231cca5aac8579b2848
Hongbusi <q951686662@qq.com>
2022-01-18 14:30:04 +0800

    chore: compiler

--- the-super-tiny-compiler.js
+++ the-super-tiny-compiler.js
@@ -422,45 +422,24 @@ function codeGenerator(node) {
 }
 
 /**
- * ============================================================================
- *                                  (۶* ‘ヮ’)۶”
- *                         !!!!!!!!THE COMPILER!!!!!!!!
- * ============================================================================
- */
-
-/**
- * FINALLY! We'll create our `compiler` function. Here we will link together
- * every part of the pipeline.
- *
  *   1. input  => tokenizer   => tokens
  *   2. tokens => parser      => ast
  *   3. ast    => transformer => newAst
  *   4. newAst => generator   => output
  */
-
 function compiler(input) {
   let tokens = tokenizer(input);
   let ast    = parser(tokens);
   let newAst = transformer(ast);
   let output = codeGenerator(newAst);
-
-  // and simply return the output!
   return output;
 }
 
-/**
- * ============================================================================
- *                                   (๑˃̵ᴗ˂̵)و
- * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!YOU MADE IT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
- * ============================================================================
- */
-
-// Now I'm just exporting everything...
 module.exports = {
   tokenizer,
   parser,
   traverser,
   transformer,
   codeGenerator,
-  compiler,
+  compiler
 };
