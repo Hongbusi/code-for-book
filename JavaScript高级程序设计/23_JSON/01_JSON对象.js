b36c9fed2ad96f1a5bfd7d36958f0fc2fea7d3c7
Hongbusi <q951686662@qq.com>
2022-05-01 16:57:33 +0800

    chore: update

---
+++ JavaScript高级程序设计/23_JSON/01_JSON对象.js
@@ -0,0 +1,13 @@
+const book = {
+  title: 'JavaScript 高级程序设计',
+  authors: ['Nicholas C. Zakas', 'Matt Frisbie'],
+  edition: 4,
+  year: 2017
+}
+
+const jsonText = JSON.stringify(book)
+
+const bookCopy = JSON.parse(jsonText)
+
+console.log(jsonText)
+console.log(bookCopy)
--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 - [Vue.js 的设计与实现](./design-and-implementation-of-vue)
 - [你不知道的 JavaScript 上卷](./you-dont-know-js-1)
+
+## JavaScript 高级程序设计（第 4 版）
+
+- [第 23 章 JSON](./JavaScript%E9%AB%98%E7%BA%A7%E7%A8%8B%E5%BA%8F%E8%AE%BE%E8%AE%A1/23_JSON)
